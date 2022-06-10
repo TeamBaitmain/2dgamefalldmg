68bd2fa58903ecccf33cf9e26a7e3403a5f7cef3
xBosse <91102761+xBosse@users.noreply.github.com>
2022-06-11 00:50:01 +0200

    Basic Movement
    Jump-Mechanik funktioniert,
    
    jump UI muss noch gescripted werden

---
+++ Assets/Scripts/JumpChargeDisplay.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class JumpChargeDisplay : MonoBehaviour
+{
+
+    public TMP_Text jumpChargeText;
+    public Image jumpChargeBar;
+
+    public PlayerMovement playerMovement;
+
+    float jumpCharge = 0f;
+    float maxJumpCharge = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        jumpChargeText.text = "Charge: " + jumpCharge + "%";
+
+
+        jumpCharge = playerMovement.jumpCharge;
+
+        BarFiller();
+
+    }
+
+    void BarFiller()
+    {
+        jumpChargeBar.fillAmount = jumpCharge / maxJumpCharge;
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public Slider jumpChargeSlider;
+
+
+    float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    
+    bool jump = false;
+
+    bool chargingJump = false;
+
+    
+
+    private float jumpForce = 0f;
+
+    public float jumpChargeUnit = 0.1f;
+
+    public float jumpCharge = 0f;  // Sprunghoehe, wird mit delta zw pressed und  released Jump aufgeladen, min und max einbauen!
+    public float maxJumpCharge = 5f;
+    public float minJumpCharge = 0.25f;
+    float jumpDir = 0f; // Bestimmt die Richtung des Sprunges, vllt ueber horizontal force regeln??
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jumpForce = controller.m_JumpForce;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if ( Input.GetButtonDown("Jump"))
+        {
+            chargingJump = true;
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            jump = true;
+        }
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        if ( !chargingJump && !jump)
+        {
+            controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        }
+
+        if (jump)
+        {
+            Debug.Log("JUMP!!!!!!");
+
+            if ( jumpCharge > maxJumpCharge)
+            {
+                jumpCharge = maxJumpCharge;
+            }
+            if (jumpCharge < minJumpCharge)
+            {
+                jumpCharge = minJumpCharge;
+            }
+            controller.m_JumpForce = jumpForce * jumpCharge;
+
+            controller.Move(horizontalMove * Time.fixedDeltaTime * jumpCharge, false, true);
+
+            chargingJump = false;
+            jumpCharge = 0f;
+        }
+
+        jump = false;
+
+        if (chargingJump && jumpCharge < maxJumpCharge)
+        {
+            jumpCharge += jumpChargeUnit;
+
+
+            
+            //jumpChargeSlider.value = jumpCharge * 50;
+
+        }
+        
+    }
+}
